4b942f6857f0de1f6a9c30720d552cbc5835b7e8
Adriano Monecchi <fornacciari@gmail.com>
2018-01-18 21:46:20 -0200

    Initial Realease
    Initial release for CMB2 Image Select Field Type plugin.

---
+++ cmb2-image-select-field-type.php
@@ -0,0 +1,112 @@
+<?php
+/*
+Plugin Name: CMB2 Image Select Field Type
+Plugin URI: https://github.com/monecchi/cmb2-image-select-field-type
+GitHub Plugin URI: https://github.com/monecchi/cmb2-image-select-field-type
+Description: Image Select field type for CMB2.
+Version: 1.0.0
+Author: Adriano Monecchi
+Author URI: https://twitter.com/dico_monecchi/
+Contributors: designroom
+License: GPLv2+
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/**
+ * Class PR_CMB2_Image_Select_Field
+ */
+class PR_CMB2_Image_Select_Field {
+
+	/**
+	 * Current version number
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Initialize the plugin by hooking into CMB2
+	 */
+	public function __construct() {
+	    add_action( 'init', array( $this, 'init_setup' ), 10, 5 );
+		add_action( 'cmb2_render_image_select', array( $this, 'cmb2_render_image_select' ), 10, 5 );
+	}
+
+	/**
+	 * Setup Scripts
+	 */
+	public function init_setup() {
+
+		$this->setup_admin_scripts();	
+
+	}
+
+	/**
+	 * Render Image Select Field
+	 */
+	public function cmb2_render_image_select( $field, $escaped_value, $object_id, $object_type, $field_type_object ) {	
+
+		if ( version_compare( CMB2_VERSION, '2.2.2', '>=' ) ) {
+			$field_type_object->type = new PR_CMB2_Image_Select_Field( $field_type_object );
+		}
+
+		$conditional_value =(isset($field->args['attributes']['data-conditional-value'])?'data-conditional-value="' .esc_attr($field->args['attributes']['data-conditional-value']).'"':'');
+		$conditional_id =(isset($field->args['attributes']['data-conditional-id'])?' data-conditional-id="'.esc_attr($field->args['attributes']['data-conditional-id']).'"':'');
+
+		$default_value = $field->args['attributes']['default'];  
+
+		$image_select = '<ul id="cmb2-image-select'.$field->args['_id'].'" class="cmb2-image-select-list">';
+
+		foreach ( $field->options() as $value => $item ) {
+
+			$selected = ( $value === ( $escaped_value =='' ? $default_value : $escaped_value ) ) ? 'checked="checked"' : '';
+
+			$image_select .= '<li class="cmb2-image-select '.($selected!= ''?'cmb2-image-select-selected':'').'">
+				<label for="' . $field->args['_id'] . esc_attr( $value ) . '">
+				<input '.$conditional_value.$conditional_id.' type="radio" id="'. $field->args['_id'] . esc_attr( $value ) . '" name="' . $field->args['_name'] . '" value="' . esc_attr( $value ) . '" ' . $selected . ' class="cmb2-option"><img class="" style=" width: auto; " alt="' . $item['alt'] . '" src="' . $item['img'] . '">
+				<br>
+				<span>' . esc_html( $item['title'] ) . '</span>
+				</label>
+				</li>';
+		}
+
+		$image_select .= '</ul>';
+
+		$image_select .= $field_type_object->_desc( true );
+
+		echo $image_select;
+	}
+	
+
+	/**
+	 * Enqueue scripts and styles
+	 */
+	public function setup_admin_scripts() {
+
+		$asset_path = apply_filters( 'pr_cmb2_image_select_asset_path', plugins_url( '', __FILE__  ) );
+
+        wp_register_style( 'cmb2_imgselect-css', $asset_path . '/css/image_select_metafield.css', array(), '1.0.0' );
+		wp_enqueue_style( 'cmb2_imgselect-css', $asset_path . '/css/image_select_metafield.css', array(), self::VERSION ); // CMB2 Image_select Field Styling
+
+        wp_register_script( 'cmb2_imgselect-js', $asset_path . '/js/image_select_metafield.js', array(), '1.0.0' );
+		wp_enqueue_script( 'cmb2_imgselect-js', $asset_path . '/js/image_select_metafield.js', array( 'cmb2-scripts' ), self::VERSION ); // CMB2 Image_select Event
+
+	}
+
+	/**
+	 * Github Plugin Updater
+	 */
+	if( ! class_exists( 'Github_Updater' ) ) {
+		include_once( plugin_dir_path( __FILE__ ) . 'git-updater.php' );
+	}
+
+	$updater = new Github_Updater( __FILE__ );
+	$updater->set_username( 'monecchi' );
+	$updater->set_repository( 'FoodStack-Framework' ); 
+
+	/* Access Token for private repo */
+	//$updater->authorize( '' ); // Your auth code goes here for private repos
+
+	$updater->initialize();
+
+}
+
+$pr_cmb2_image_select_field = new PR_CMB2_Image_Select_Field();
---
+++ readme.md
@@ -0,0 +1,60 @@
+# CMB2 Image Select Field Type.
+
+Enables a custom `image_select` field type for CMB2.
+
+It acts pretty much the same as a radio input, prettier though, by adding Visual Interest using images instead.
+
+<b>What you're getting:</b>
+An alternate version of **[CMB2-Image_Select-Field-Type](https://github.com/improy/CMB2-Image_Select-Field-Type)** by [improy](https://github.com/improy). It was re-built as a WordPress plugin so you can easily install it & keep it updated.
+
+
+## Installation & Updates
+
+In order to get automatic plugin updates, please follow the WordPress [Manual Plugin Installation](https://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation) guide. 
+
+This means you'd simply need to download the plugin from Github as a .zip file and install it the WordPress way.
+
+
+## Usage
+
+- Requires [CMB2](https://github.com/CMB2/CMB2) to be installed as a standalone plugin or integrated in a theme or plugin.
+
+`image_select` as a field type.
+
+```php
+array(	
+    'name' => __('Image Select', 'cmb2'),
+    'desc' => __('page layout using image_select', 'cmb2'),
+    'id'      => $prefix . 'page_custom_layout',
+    'type' => 'image_select',
+    'options' => array(
+        'disabled' => array('title' => 'Full Width', 'alt' => 'Full Width', 'img' => $image_path . 'img/sidebar-disabled.gif'),
+        'sidebar-left' => array('title' => 'Sidebar Left', 'alt' => 'Sidebar Left', 'img' => $image_path . 'img/sidebar-left.gif'),
+        'sidebar-right' => array('title' => 'Sidebar Right', 'alt' => 'Sidebar Right', 'img' => $image_path . 'img/sidebar-right.gif'),
+        'sidebar-leftright' => array('title' => 'Both Sidebars', 'alt' => 'Both Sidebars', 'img' => $image_path . 'img/sidebar-both.gif'),
+    ),
+    'default' => 'disabled',    
+)
+```
+
+## Integrations
+Ready to be integrated within any Theme or Plugin.
+Install it as an ordinary WordPress plugin or include it within your theme's or plugin's code by requiring it as follows.
+
+```
+    // Check for the plugin's class
+    if(!( class_exists( 'PR_CMB2_Image_Select_Field' ) ) ) {
+        require_once( $plugin_or_theme_path . 'cmb2-image-select-field-type.php' );
+    }
+}
+```
+
+By choosing this method, it requires you to manually keep the plugin files up to date as you'd no longer get automatic updates. 
+
+## Screenshots
+<img src="https://raw.githubusercontent.com/improy/CMB2-Image_Select-Field-Type/master/CMB2-Image_select-Field-Type.jpg" alt="CMB2 Image_Select Field Type"/>
+
+### Tutorial on how to integrate image_select field on a WordPress theme
+
+_Original Method_ - No need to follow it up if you're using it as a standalone plugin.
+http://www.proy.info/how-to-create-cmb2-image-select-field-type/
